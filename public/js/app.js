82723d14d383dc78f1a106a125995f2539e07dae
Hafsa <moussaidhafsa2@gmail.com>
2025-04-18 10:42:31 +0100

    spliting the parts of the project with comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,38 @@
 
+// the user should choose the option that he wants access to:
+
+
+
+
+
+
+
+// Option: SIGN UP;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Option: Log In;
+
+
+
+
+
+
+
+
+
+// Option:  changing Password ; 
+
 
 
 
